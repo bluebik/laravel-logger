38d5995e7d31bca130f3d41477f6ee128a30c23e
Puthipong <puthipong@bluebik.com>
2021-08-25 10:58:33 +0700

    add CommandLogger.php

---
+++ src/Traits/CommandLogger.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use Bluebik\Logger\LoggerFactory;
+use Illuminate\Support\Str;
+
+trait CommandLogger
+{
+
+    protected $logger;
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->logger = LoggerFactory::create('command');
+        $this->logger->setRequestId(time() . Str::random(6));
+    }
+}
